ec17d5adcbd372f3139a4105b44df2119841f733
Marc Kupietz <kupietz@ids-mannheim.de>
2024-04-26 17:02:53 +0200

    Add publication year to corpus title
    Resolves #13

--- test/test-xml.sh
+++ test/test-xml.sh
@@ -17,7 +17,7 @@ observed=$(xmlstarlet  sel --net -t -v "count(//idsText)"  $I5_FILE)
 if $(assert_eq "$observed" "$TEXTS"); then
   log_success "$I5_FILE contains $TEXTS idsText elements"
 else
-  log_failure "$I5_FILE does not contain $TEXTS idsText elements"
+  log_failure "$I5_FILE does not contain $TEXTS idsText elements, but: $observed"
 fi
 
 
@@ -25,12 +25,19 @@ observed=$(xmlstarlet sel --net -t -v "count(/idsCorpus/idsDoc/idsText/idsHeader
 if $(assert_eq "$observed" "$TEXTS"); then
   log_success "$I5_FILE contains $TEXTS non-empty h.author elements"
 else
-  log_failure "$I5_FILE does not contain $TEXTS non-empty h.author elements"
+  log_failure "$I5_FILE does not contain $TEXTS non-empty h.author elements: $observed"
+fi
+
+observed=$(xmlstarlet sel --net -t -v "/idsCorpus/idsHeader/fileDesc/titleStmt/c.title" target/dnb13.i5.xml)
+if $(assert_eq "$observed" "Deutsche Nationalbibliothek: Belletristik 2013"); then
+  log_success "c.title contains yeaar"
+else
+  log_failure "c.title does not contain year: $observed"
 fi
 
 observed=$(xmlstarlet sel --net -t -v "count(/idsCorpus/idsDoc/idsText/idsHeader/fileDesc/sourceDesc/biblStruct/monogr/h.author[contains(., '[')])"  target/dnb13.i5.xml)
 if $(assert_eq "$observed" "0"); then
   log_success "authors do not contain []"
 else
-  log_failure "authors contain []"
+  log_failure "authors contain []: $observed"
 fi
